bee4a5ca614a8dc7bbcf092f76256d97a401913f
ayushbhatt1108 <ayushbhatt335@gmail.com>
2025-04-14 20:33:06 +0530

    java

---
+++ 01_HelloWorld/Variable.java
@@ -0,0 +1,12 @@
+public class Variable{
+
+    public static void main(String[] args) {
+        int num1 = 8; // first datatype is specified then variable name was assigned and then we gave a 
+                      // value to variable
+        int num2 = 7;
+        int multiply = num1*num2;
+        System.out.println(multiply);
+        
+
+    }
+}
